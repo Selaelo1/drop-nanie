f33a8e20b2452239ee570042b7c69b78c88f2f21
Selaelo2 <selaelolanga10@gmail.com>
2025-07-30 16:09:21 +0200

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Hero from "./components/Hero";
+import EmailSubscribe from "./components/EmailSubscribe";
+import DropTeaser from "./components/DropTeaser";
+import About from "./components/About";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <Hero />
+      <EmailSubscribe />
+      <DropTeaser />
+      <About />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
